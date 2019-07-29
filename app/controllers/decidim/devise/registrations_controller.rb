d182bc877d9d60443b8a682858f0f7692992cc86
ailobe <aitorlopezbeltran@gmail.com>
2019-07-29 11:19:18 +0200

    Refactor registrations controller

--- app/controllers/decidim/devise/registrations_controller.rb
+++ app/controllers/decidim/devise/registrations_controller.rb
@@ -15,43 +15,34 @@ module Decidim
       invisible_captcha
 
       def new
-        @form = form(Decidim::Erc::CrmAuthenticable::RegistrationFormStepOneForm).from_params(
+        @form = form(Decidim::Erc::CrmAuthenticable::IdentityDocumentForm).from_params(
             user: {
               sign_up_as: "user"
             }
           )
       end
-      
+
       def new_step_2
-        @form = form(Decidim::Erc::CrmAuthenticable::RegistrationFormStepOneForm).from_params(params[:user])
-        
+        @form = form(Decidim::Erc::CrmAuthenticable::IdentityDocumentForm).from_params(params[:user])
+
         if @form.valid?
-          @form = form(Decidim::Erc::CrmAuthenticable::RegistrationFormStepTwoForm).from_params(
+          @form = form(Decidim::RegistrationForm).from_params(
             user: {
-              sign_up_as: "user",
-            }.merge(extra_params)
+              sign_up_as: "user"
+            }.merge(@form.registration_form_params)
           )
-          flash[:notice] =  "S'ha trobat l'usuari"
         else
-          render :new
-          flash[:alert] =  "No s'ha pogut processar la solicitud"
+          return render :new
         end
       end
 
       def create
-        @form = form(Decidim::Erc::CrmAuthenticable::RegistrationFormStepTwoForm).from_params(params[:user])
-        
+        @form = form(Decidim::RegistrationForm).from_params(params[:user])
+
         CreateRegistration.call(@form) do
           on(:ok) do |user|
-            if user.active_for_authentication?
-              set_flash_message! :notice, :signed_up
-              sign_up(:user, user)
-              respond_with user, location: after_sign_up_path_for(user)
-            else
-              set_flash_message! :notice, :"signed_up_but_#{user.inactive_message}"
-              expire_data_after_sign_in!
-              respond_with user, location: after_inactive_sign_up_path_for(user)
-            end
+            set_flash_message! :notice, :signed_up
+            respond_with user, location: after_sign_up_path_for(user)
           end
 
           on(:invalid) do
@@ -74,11 +65,6 @@ module Decidim
       def build_resource(hash = nil)
         super(hash)
         resource.organization = current_organization
-      end
-
-      def extra_params
-        return {} unless @form.data.present?
-        @form.data
       end
     end
   end
