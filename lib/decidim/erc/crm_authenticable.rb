d13bfc8a495a7d3e2d0ff59c4e6b2b3026f2897f
ailobe <aitorlopezbeltran@gmail.com>
2019-10-11 12:02:32 +0200

    Change: Scopes Mapping

--- lib/decidim/erc/crm_authenticable.rb
+++ lib/decidim/erc/crm_authenticable.rb
@@ -6,6 +6,11 @@ require "decidim/erc/crm_authenticable/workflow"
 module Decidim
   module Erc
     module CrmAuthenticable
+      # Names to filter CiviCRM Contacts of type 'Organization' and sub_type 'Comarcal'
+      # Used to generate the scopes mapping that is assigned to SCOPE_CODES.
+      # See lib/tasks/civi_crm.rake
+      # Must be set up via initializer.
+      CIVICRM_COMARCAL_EXCEPTIONS = Array.new
       SCOPE_CODES = { "custom_21" => "custom_21" }.freeze if Rails.env.test?
 
       autoload :Log, "decidim/erc/crm_authenticable/log"
