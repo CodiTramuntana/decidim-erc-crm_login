77d56b7ec2cdfca39b9f1699e5e2ddc4f919ff02
ailobe <aitorlopezbeltran@gmail.com>
2019-11-14 10:27:15 +0100

    Fix: disambiguate nicknames

---
+++ app/extensions/decidim/nicknamizable_extension.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# Module to add a new method to override Nicknamizable::nicknamize
+module NicknamizableExtension
+  def self.included(base)
+    base.extend ClassMethods
+  end
+
+  module ClassMethods
+    # Disambiguates nicknames, scoped to the organization.
+    # The original method expects a name to convert to a nickname; but since
+    # we already have a nickname, we only need the disambiguation logic.
+    def nicknamize(nickname, scope = {})
+      return unless nickname
+
+      disambiguate(nickname, scope)
+    end
+  end
+end
